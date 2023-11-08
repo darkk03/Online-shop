23af6335ffb8ee9978d8bb615167e22a34c1ecf2
darkk03 <towboatqq@gmail.com>
2023-11-09 00:11:49 +0100

    'cart-css-update'

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeFromCart, increaseQuantity, decreaseQuantity } from "../../feauters/actions/actions";
 import cartReducer from "../../feauters/actions/actionsSlice";
+import "../../styles/cart.css"
+import sprite from '../../images/sprite.svg';
 
 const Cart = () => {
     const cart = useSelector((state) => state.cart.cart);
@@ -23,8 +25,10 @@ const Cart = () => {
         return cart.reduce((total, item) => total + item.price * item.quantity, 0);
     };
 
+
     return (
-        <div>
+        <div className="cart_">
+            <div className="cart-content">
             <h2 className="cart-title">
                 Your Cart
             </h2>
@@ -36,10 +40,10 @@ const Cart = () => {
 
                         return (
                             <div className="item" key={id}>
-                                <div className="image">
-                                    <img src={images[0]} alt="Product Image" className="image_producs" />
+                                <div className="image_">
+                                    <img src={images[0]} alt="Product Image" className="image_producs_" />
                                 </div>
-                                <div>
+                                <div className="block-name-cat">
                                     <div className="name">{title}</div>
                                     <div className="category">{category.name}</div>
                                 </div>
@@ -48,21 +52,23 @@ const Cart = () => {
                                 <div className="quantity">
                                     <div className="minus">
                                         <svg className="icon">
-                                            <use xlinkHref="/sprite.svg#plus" />
+                                            <use xlinkHref={`${sprite}#minus`} />
                                         </svg>
                                     </div>
-                                    <span>{quantity}</span>
+
+                                    <span>{quantity !== undefined ? quantity : 1}</span>
+
                                     <div className="plus">
                                         <svg className="icon">
-                                            <use xlinkHref="/sprite.svg#plus" />
+                                            <use xlinkHref={`${sprite}#plus`} />
                                         </svg>
                                     </div>
                                 </div>
-                                <div className="total">{price * quantity}</div>
+                                <div className="total">{(price !== undefined && quantity !== undefined) ? price * quantity : 0}</div>
 
                                 <div className="close">
                                     <svg className="icon">
-                                        <use xlinkHref="/sprite.svg#plus" />
+                                        <use xlinkHref={`${sprite}#plus`} />
                                     </svg>
                                 </div>
                             </div>
@@ -70,12 +76,13 @@ const Cart = () => {
                     })}
                 </div>
                 <div className="actions">
-                    <div className="total">
-                        TOTAL PRICE:{" "}
-                        <span>
-                            {/* {sumBy(cart.map(({ quantity, price }) => quantity * price))}$ */}
-                        </span>
-                    </div>
+                <div className="total">
+                TOTAL PRICE:{" "}
+                    <span>
+                        {getTotalPrice}
+                    </span>
+                 </div>
+
 
                     <button className="proceed">Proceed to checkout</button>
                 </div>
@@ -83,6 +90,7 @@ const Cart = () => {
             ) : (
                 <div className="empty-title">Here is empty</div>
             )}
+            </div>
         </div>
     )
 }
