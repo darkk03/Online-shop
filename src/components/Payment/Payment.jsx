8849274b0a1c6d479a95af37cf0101d80dc492b4
darkk03 <towboatqq@gmail.com>
2023-11-12 02:28:54 +0100

    'css-card-update'

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Cards from "react-credit-cards";
 import "../../styles/payment.css"
-
+import card_photo from "../../images/6.jpeg"
 
 
 
@@ -17,77 +17,94 @@ const Payment = () => {
     return (
         <>
         {/* <div className="rccs__card backcolor"> */}
-  
-        <div clasName="rccs__card rccs__card--unknown">
-          <Cards
-            number={number}
-            name={name}
-            expiry={date}
-            cvc={cvc}
-            focused={focus}
-          />
+
+        <div className="rccs__card">
+          <div className="rccs__card-content">
+            <div className="card-back">
+
+            <Cards
+              number={number}
+              name={name}
+              expiry={date}
+              cvc={cvc}
+              focused={focus}
+            />
+          </div>
+            </div>
+
         </div>
   
         <br />
-        <div className="credit-card-content">
-          <div className="row">
-            <div className="col-sm-11">
-              <label for="name">Card Number</label>
-              <input
+        <div className="credit-card">
+          <div className="credit-card-content">
+            <div className="row">
+              <div className="col-sm-11">
+                <label for="name">Card Number</label>
+                <input
                 type="text"
-                className="form-control"
+                className="form-control1"
                 value={number}
                 name="number"
                 onChange={(e) => {
-                  SetNumber(e.target.value);
+                  if (e.target.value.length <= 16) {
+                    SetNumber(e.target.value);
+                  }
                 }}
                 onFocus={(e) => SetFocus(e.target.name)}
-              ></input>
+                maxLength="16"
+              />
+              </div>
             </div>
-          </div>
-          <br />
-          <div className="row">
-            <div className="col-sm-11">
-              <label for="name">Card Name</label>
-              <input
-                type="text"
-                className="form-control"
-                value={name}
-                name="name"
-                onChange={(e) => {
-                  SetName(e.target.value);
-                }}
-                onFocus={(e) => SetFocus(e.target.name)}
-              ></input>
+            <br />
+            <div className="row">
+              <div className="col-sm-11">
+                <label for="name">Card Name</label>
+                <input
+                  type="text"
+                  className="form-control2"
+                  value={name}
+                  name="name"
+                  onChange={(e) => {
+                    SetName(e.target.value);
+                  }}
+                  onFocus={(e) => SetFocus(e.target.name)}
+                ></input>
+              </div>
             </div>
-          </div>
-          <br />
-          <div className="row">
-            <div className="col-sm-6">
-              <label for="name">Expiration Date</label>
-              <input
-                type="text"
-                name="expiry"
-                className="form-control"
-                value={date}
-                onChange={(e) => {
-                  SetDate(e.target.value);
-                }}
-                onFocus={(e) => SetFocus(e.target.name)}
-              ></input>
-            </div>
-            <div className="col-sm-5">
-              <label for="name">CVV</label>
-              <input
-                type="tel"
-                name="cvc"
-                className="card"
-                value={cvc}
-                onChange={(e) => {
-                  SetCvc(e.target.value);
-                }}
-                onFocus={(e) => SetFocus(e.target.name)}
-              ></input>
+            <br />
+            <div className="row">
+              <div className="col-sm-6">
+                <label for="name">Expiration Date</label>
+                <input
+                  type="text"
+                  name="expiry"
+                  className="form-control"
+                  value={date}
+                  onChange={(e) => {
+                    if (e.target.value.length <= 4) {
+                      SetDate(e.target.value);
+                    }
+                  }}
+                  onFocus={(e) => SetFocus(e.target.name)}
+                  maxLength="4"
+                />
+              </div>
+              <div className="col-sm-5">
+                <label for="name">CVV</label>
+                <input
+                  type="tel"
+                  name="cvc"
+                  className="card"
+                  value={cvc}
+                  onChange={(e) => {
+                    if (e.target.value.length <= 3) {
+                      SetCvc(e.target.value);
+                    }
+                  }}
+                  onFocus={(e) => SetFocus(e.target.name)}
+                  maxLength="3"
+                />
+              </div>
             </div>
           </div>
         </div>
