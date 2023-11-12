46ccfebf0f1aacf4797c0f96e9c2f0e9c6220f58
darkk03 <towboatqq@gmail.com>
2023-11-12 20:57:41 +0100

    'fav-css-update'

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import sprite from '../../images/sprite.svg';
 import { removeFromFavorite } from "../../feauters/actions/actions"; 
+import "../../styles/favorites.css"
 
 const Favorites = () => {
     const favorite = useSelector((state) => state.cart.favorite);
@@ -14,34 +15,40 @@ const Favorites = () => {
     };
 
     return (
-        <div className="Products">
-            <div className="Products-content">
+        <div className="favorites-fav">
+            <div className="favorites-content">
                 <h2 className="cart-title">
                     Favorite list
                 </h2>
                 {favorite && favorite.length > 0 ? (
                     <>
-                        <div className="list">
+                        <div className="fav-list">
                             {favorite.map((item) => {
                                 const { title, category, images, price, id } = item;
 
                                 return (
-                                    <div className="item" key={id}>
-                                        <div className="image_">
-                                            <img src={images[0]} alt="Product Image" className="image_producs_" />
-                                        </div>
-                                        <div className="fav-price">
-                                            {price}
-                                        </div>
-
-                                        <div className="block-name-cat">
-                                            <div className="name">{title}</div>
-                                            <div className="category">{category.name}</div>
-                                        </div>
-                                        <div className="close" onClick={() => handleRemoveFromFavorite(id)}>
-                                            <svg className="icon-close_">
+                                    <div className="fav-item" key={id}>
+                                        <div className="close-fav" onClick={() => handleRemoveFromFavorite(id)}>
+                                            <svg className="icon-close_-fav">
                                                 <use xlinkHref={`${sprite}#plus`} />
                                             </svg>
+                                        </div>
+                                        <div className="fav-image_">
+                                            <img src={images[0]} alt="Product Image" className="-fav-image_producs_" />
+                                        </div>
+                                        <div className="block-cat-fav">
+                                            <div className="category-fav">{category.name}</div>
+                                        </div>
+                                        <div className="block-name-fav">
+                                            <div className="name-fav">{title}</div>
+                                        </div>
+                                        <div className="info-fav">
+                                            <div className="fav-price">
+                                                {price}$
+                                            </div>
+                                            <div className="purchases-fav">
+                                                {Math.floor( Math.random() * 20 + 1)} purchased 
+                                            </div>
                                         </div>
                                     </div>
                                 );
